8d643b5c3c27666c0f29d828ab88e0c2740dc03a
darren3481 <96778545+darren3481@users.noreply.github.com>
2026-03-10 09:58:32 +0800

    UPDATE

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+
+int hampir_sama(float x, float y) {
+    float selisih = fabs(x - y);
+    float toleransi = 0.01 * fmax(x, y);
+    return selisih <= toleransi;
+}
 
 int main() {
+    // BILANGAN BULAT
     int a, b, c;
     int max;
 
@@ -39,6 +47,46 @@ int main() {
         printf("Segitiga Siku-Siku\n");
     }
     else if (a == b || b == c || a == c) {
+        printf("Segitiga Sama Kaki\n");
+    }
+    else {
+        printf("Segitiga Bebas\n");
+    }
+
+    // BILANGAN PECAHAN
+    float a, b, c;
+    float max;
+
+    printf("Masukkan tiga bilangan pecahan: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("Tidak dapat membentuk segitiga\n");
+        return 0;
+    }
+
+    // Cari sisi terbesar
+    max = fmax(a, fmax(b, c));
+
+    float sum_dua;
+    if (max == a) sum_dua = b + c;
+    else if (max == b) sum_dua = a + c;
+    else sum_dua = a + b;
+
+    if (max >= sum_dua) {
+        printf("Tidak dapat membentuk segitiga\n");
+        return 0;
+    }
+
+    if (hampir_sama(a,b) && hampir_sama(b,c)) {
+        printf("Segitiga Sama Sisi\n");
+    }
+    else if (hampir_sama(a*a + b*b, c*c) ||
+             hampir_sama(a*a + c*c, b*b) ||
+             hampir_sama(b*b + c*c, a*a)) {
+        printf("Segitiga Siku-Siku\n");
+    }
+    else if (hampir_sama(a,b) || hampir_sama(b,c) || hampir_sama(a,c)) {
         printf("Segitiga Sama Kaki\n");
     }
     else {
